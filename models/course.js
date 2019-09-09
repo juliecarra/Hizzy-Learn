bd87adccdc9b5d8271b82b129dd85942f163ee7e
Loup A <loup.aparicio@gmail.com>
2019-09-09 17:28:33 +0200

    add manage-all tab

--- models/course.js
+++ models/course.js
@@ -3,5 +3,10 @@ const Schema = mongoose.Schema;
 
 const courseSchema = new Schema({
   course_name: { type: String, required: true },
+  course_difficulty: { type: String, required: true },
   course_videos: [{ type: Schema.Types.ObjectId, ref: "Videos" }]
 });
+
+const courseModel = mongoose.model("Courses", courseSchema);
+
+module.exports = courseModel;
