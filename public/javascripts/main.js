f28edb7c1d96953de85d39bec0bd67c4c9a64c54
Loup A <loup.aparicio@gmail.com>
2019-09-11 10:13:56 +0200

    ongoing

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,25 @@
-const fileSelect = (e) => {
-  $('.fileName').text(e.target.files[0].name);
+const fileSelect = e => {
+  $(".fileName").text(e.target.files[0].name);
+};
+
+function addViewedVideo(evt) {
+  console.log(evt.target.id);
+  const id = evt.target.id;
+  axiosHandler
+    .patch("/profile", { toto: id })
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
+}
+function removeViewedVideo(evt) {
+  console.log(evt.target.id);
+}
+
+const addViewedBtn = document.getElementsByClassName("add-viewed-btn");
+const removeViewedBtn = document.getElementsByClassName("remove-viewed-btn");
+
+for (let btn of addViewedBtn) {
+  btn.onclick = addViewedVideo;
+}
+for (let btn of removeViewedBtn) {
+  btn.onclick = removeViewedVideo;
 }
--- routes/profile.js
+++ routes/profile.js
@@ -1,6 +1,16 @@
 const express = require("express");
-const router = express.Router();
+const router = new express.Router();
 const videoModel = require("../models/video");
 const courseModel = require("../models/course");
+const userModel = require("../models/user");
+
+router.patch("/profile/", (req, res) => {
+  console.log(req.body);
+  console.log(req.user);
+  userModel
+    .findOneAndUpdate({ _id: req.user._id })
+    .then()
+    .catch();
+});
 
 module.exports = router;
